a9d484b855bfa81a524cba1438fb227a83868944
zxTitann <wallacewinicius929@gmail.com>
2025-05-18 18:05:01 -0300

    Deleta o diretório .src/Graficos

--- .src/Graficos/Graficos_ManimCE/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Configurações básicas do Manim
-MANIM_DIR = os.path.dirname(__file__) #definindo o diretório do Manim, o __file__ é o arquivo atual
-OUTPUT_DIR = os.path.join(MANIM_DIR, "output") #definindo o diretório de saída, o join é para juntar os diretórios
